7ef4198b0e1dd1d6e5c671f258a0180fec39b741
Tyler Reddy <treddy@lanl.gov>
2024-06-27 16:24:38 -0600

    ENH: draft of fast script
    * Early draft of a "fast" bubble detection script,
    designed to be run on a single image at a time for
    fast iteration/improvement on bubble detection
    methods.

---
+++ neat_ml/misc/single_image_iterate.py
@@ -0,0 +1,48 @@
+"""
+The purpose of this module is to replicate key
+parts of the main image analysis workflow, but
+apply them only to a single plate reader image
+for faster iteration/improvement of bubble
+detection/analysis methods.
+"""
+
+import argparse
+
+import cv2
+import skimage
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+
+def opencv_blob_detection_single_image(image_path, debug: bool = False):
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    params = cv2.SimpleBlobDetector_Params()
+    params.filterByArea = True
+    params.minArea = 300
+    detector = cv2.SimpleBlobDetector_create(params) # type: ignore[attr-defined]
+    # actual detection of blobs happens:
+    keypoints = detector.detect(image)
+    num_blobs_img = len(keypoints)
+    if debug:
+        fig, axs = plt.subplots(1, 2, figsize=(12, 8))
+        image_orig = skimage.color.gray2rgb(image)
+        axs[0].imshow(image_orig)
+        axs[0].set_title("original")
+        blob_image = cv2.drawKeypoints(image.copy(), #type: ignore[call-overload]
+                                       keypoints,
+                                       None,
+                                       (255, 0, 0),
+                                       cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        axs[1].imshow(blob_image)
+        axs[1].set_title(f"OpenCV SimpleBlobDetector (Found {num_blobs_img} blobs)")
+        fig.savefig(f"OpenCV_blob_detection_debug.png", dpi=300)
+        matplotlib.pyplot.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image-path", type=str, help="Path to the plate reader image file to be processed")
+    args = parser.parse_args()
+    opencv_blob_detection_single_image(image_path=args.image_path,
+                                       debug=True)
